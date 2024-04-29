f47f013f2135e7e3a2aea173965cbdb3f14c03cc
Dulangi2002 <cb010550@students.apiit.lk>
2024-04-29 22:26:22 +0530

    cors5

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import got from 'got';
-import cors from 'cors'
-
+import axios from 'axios';
+import cors from 'cors';
 
 const app = express();
+app.use(cors()); // Apply CORS middleware
+
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,53 +14,37 @@ const API_SERVICE_URL = 'https://secure.myfees.lk';
 const router = express.Router();
 const PORT = process.env.PORT || 6001;
 
-
-
 app.use(router);
-app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin: *');
-  res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+  res.header('Access-Control-Allow-Origin', 'https://startling-duckanoo-c5860c.netlify.app');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-
-var corsOptions = {
-  origin: 'https://startling-duckanoo-c5860c.netlify.app',
-  optionsSuccessStatus: 200 
-}
 
 app.get('/', (req, res) => {
   res.send("Hello World");
 });
 
-
 router.post('/api', async (req, res) => {
   try {
-    const response = await got.post(`${API_SERVICE_URL}/api/sch/payments`, {
-      json: req.body,
-      responseType: 'json',
+    const response = await axios.post(`${API_SERVICE_URL}/api/sch/payments`, req.body, {
       headers: {
         'Content-Type': 'application/json',
       },
-
     });
-    
-    console.log(response.body);
-    console.log('External Server Status Code:', response.statusCode);
-    if(response.statusCode === 201){
-    const jsonRes =JSON.stringify(response.body);
-    res.send(jsonRes);
-    }else{
-      res.status(response.statusCode).json({ error: "Unexpected status code", statusCode: response.statusCode });
-    }
 
-    
+    console.log(response.data);
+    console.log('External Server Status Code:', response.status);
+    if (response.status === 201) {
+      res.json(response.data);
+    } else {
+      res.status(response.status).json({ error: "Unexpected status code", statusCode: response.status });
+    }
   } catch (error) {
     console.error(error);
     if (error.response) {
-      const statusCode = error.response.statusCode;
+      const statusCode = error.response.status;
       res.status(statusCode).json({ error: "External Server Error", statusCode });
     } else {
       res.status(500).json({ error: "Internal Server Error" });
@@ -67,12 +52,8 @@ router.post('/api', async (req, res) => {
   }
 });
 
-
-
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
 
 export default app;
