f1151407cf2ac6e4bef4e8616fe42ab485503158
Dulangi2002 <cb010550@students.apiit.lk>
2024-04-29 21:36:22 +0530

    cors2

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ const PORT = process.env.PORT || 6001;
 app.use(router);
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://startling-duckanoo-c5860c.netlify.app');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send("Hello World");
 });
