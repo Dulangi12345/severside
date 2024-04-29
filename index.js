b600cdfc10899c7bc24401c36402b34c1f339dc5
Dulangi2002 <cb010550@students.apiit.lk>
2024-04-29 22:09:41 +0530

    cors4

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ app.use(router);
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://startling-duckanoo-c5860c.netlify.app');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Origin: *');
+  res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
   next();
 });
 
